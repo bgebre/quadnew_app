12fd53aa92c34dd7d001f42259332e8f26e91112
Banna Gebre <bgebre09@gmail.com>
2014-08-24 03:58:20 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuadnewApp::Application.config.secret_key_base = 'f447047fbd48a98eebb539de3a5290851d245afba66fcbc0297bbf9df795898cdae9f6fa1e04c04356236ca96d349abec77cc5949d3e71f442026ff1c5ed0656'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QuadnewApp::Application.config.session_store :cookie_store, key: '_quadnew_app_session'
